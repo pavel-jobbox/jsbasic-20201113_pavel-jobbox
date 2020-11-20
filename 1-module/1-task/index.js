ad099c53f483346d3a5c9c3f87ecd2369d154e61
Pavel Opanasenko <pavel.jobbox@gmail.com>
2020-11-20 12:00:38 +0200

    added function factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,18 @@
  * @param {number} n
  * @returns {number}
  */
+
 function factorial(n) {
-  // ваш код...
+  let num = 1;
+  while (n >= 1) {
+    num = num * n
+    n = n - 1
+  }
+
+  return num
 }
+
+factorial(0); // 1
+factorial(1); // 1
+factorial(3); // 6
+factorial(5); // 120
