333c83ef26120578ed6c0ef507f637e8d7f38b42
Pavel Opanasenko <pavel.jobbox@gmail.com>
2020-11-22 16:19:12 +0200

    усек строку

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,7 +5,7 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-  if (str.length >= maxlength) {
+  if (str.length > maxlength) {
     let newStr = str.slice(0, maxlength - 1) + String.fromCodePoint(133);
     return newStr;
   } else {
