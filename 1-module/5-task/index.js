2be9c33b851f2024973414395b92173eb67fbd5c
Pavel Opanasenko <pavel.jobbox@gmail.com>
2020-11-22 16:36:17 +0200

    решил задание 5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -6,8 +6,8 @@
  */
 function truncate(str, maxlength) {
   if (str.length > maxlength) {
-    let newStr = str.slice(0, maxlength - 1) + String.fromCodePoint(133);
-    return newStr;
+    str = str.slice(0, maxlength - 1) + String.fromCodePoint(133);
+    return str;
   } else {
     return str;
   }
