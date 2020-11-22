5f44c84d75cfd566448730232cd3909f961df6a8
Pavel Opanasenko <pavel.jobbox@gmail.com>
2020-11-22 15:02:22 +0200

    fixed

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,9 +5,9 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-  if (str.length > maxlength) {
+  if (str.length >= maxlength) {
     return str.slice(0, maxlength - 1) + '...';
   } else {
-    return str
+    return str;
   }
 }
