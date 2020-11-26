9ff4a6b58561b65c52965c53decb79f515d1c22f
Pavel Opanasenko <pavel.jobbox@gmail.com>
2020-11-26 07:57:33 +0200

    added function salary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,18 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+
+  for (let i in salaries) {
+
+    let value = salaries[i];
+
+    if (Number(value) == Number(value)) {
+      sum = sum + value;
+    } else {
+      continue;
+    }
+  }
+
+  return sum;
 }
