d99491921d86275fa12d129c7f4e2817f7114050
Pavel Opanasenko <pavel.jobbox@gmail.com>
2020-12-05 16:13:52 +0200

    min \ max

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -5,16 +5,10 @@
  */
 function getMinMax(str) {
 
-  const arr = []
-  let x = str.split(' ').map(index => index.replace(/,/g, '')).filter(item => {
-    let n = parseFloat(item)
-    if (isNaN(n) != isNaN(NaN)) {
-      arr.push(n)
-    }
-  })
+  const nums = str.split(' ').map((val) => parseFloat(val)).filter(num => !Number.isNaN(num));
 
   return result = {
-    min: Math.min(...arr),
-    max: Math.max(...arr)
+    min: Math.min(...nums),
+    max: Math.max(...nums)
   }
 }
